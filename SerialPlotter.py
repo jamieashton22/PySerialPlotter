782644b8163cdfb7bb3def78c70006e6eaf150df
Jamie Ashton <jamie.ashton@student.manchester.ac.uk>
2025-08-06 16:10:20 +0100

    Changed class a bit, reintroduced serial stuff and reworked main logic

--- SerialPlotter.py
+++ SerialPlotter.py
@@ -5,18 +5,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
 from matplotlib.lines import Line2D
-
+import sys
+import serial
+import serial.tools.list_ports
+import time
 
 # --------- SERIALPLOTTER CLASS ---------------------------------
 
 class SerialPlotter: 
-    def __init__(self, ax, twidth, dt):                                     # ax - axis object where line will be plotted, twidth - length of time window for x axis, dt - time incr. between data points
+
+    def __init__(self, ax, twidth):                                     # ax - axis object where line will be plotted, twidth - length of time window for x axis, dt - time incr. between data points
        
         self.ax = ax
         self.twidth = twidth
-        self.dt = dt
 
         self.tdata = [0] # to hold time values
         self.ydata = [0] # to hold sensor values
@@ -26,8 +28,7 @@ class SerialPlotter:
         self.ax.set_ylim()      # add y limits
         self.ax.set_xlim(0, twidth)      # add x limits 
 
-    def update(self, y):            # method to add a y-value to the plot and update it 
-        t = self.tdata[-1] + self.dt
+    def update(self, y, t):            # method to add a y-value to the plot and update it 
 
         self.tdata.append(t)
         self.ydata.append(y)
@@ -35,6 +36,57 @@ class SerialPlotter:
         return self.line,
 
 
+# -------- SELECTING AND OPENING SERIAL PORT ------------------------------------------------
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+port = ''
+portList = []
+
+print("\n List of available ports \n")
+for onePort in ports:
+    portList.append(str(onePort.device))
+    print(str(onePort))
+
+port = input("\n select port \n")
+
+if port not in portList:
+    print("Invalid port chosen")
+    sys.exit(1)
+
+print("\n Port selected: ")
+print(port)
+
+# OPEN PORT
+
+#get baud rate from user
+chosenBaud = input("\n select baud rate \n")
+serialInst.baudrate = chosenBaud
+
+serialInst.port = port
+serialInst.open()
+print("\n port open \n")
 
 # -------- MAIN ------------------------------------------------
 
+fig, ax = plt.subplots()
+plotter = SerialPlotter(ax,10)
+
+start_time = time.time()
+
+while True:
+
+    if serialInst.in_waiting:
+
+        current_time = time.time()
+        time_elapsed = current_time - start_time        # get t values for x axis
+
+        data_raw = serialInst.readline()
+        data_strip=data_raw.decode('utf-8').rstrip()
+        sensor_reading = float(data_strip)    # get sensor reading for y axis 
+
+        plotter.update(sensor_reading, time_elapsed)
+        plt.pause(0.01)
+
+        
+        
